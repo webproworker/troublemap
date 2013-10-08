ef71879d109e9b38b1b23ebfd4d266b6eba8224b
webproworker <webproworker@gmail.com>
2013-10-08 13:39:05 +0300

    tweaked the map on the front page

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Troublemap::Application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  # get 'troubles/:id/city' => 'troubles#city', as: :city
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
