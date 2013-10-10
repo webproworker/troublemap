d8fdd8bd213d0083fc11301f863b8d8b65065f37
webproworker <webproworker@gmail.com>
2013-10-10 13:21:41 +0300

    changed the routes. made the :id/city be city/:id (#7)

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,12 @@ class User < ActiveRecord::Base
   
   def self.authenticate(email, password)
     user = User.where(email: email).first
-    throw Exception.new(user.password_hash)
-    # throw Exception.new(BCrypt::Engine.hash_secret(password, user.password_salt))
+    # throw Exception.new(user.password_hash) #uncaught throw #<Exception: $2a$10$9FHhPyb7BW01ktwTTgZHX.hlKKv4ajX/dX9D/xNGmZoajJTdGG4N.>
+    # throw Exception.new(user.password_salt) #uncaught throw #<Exception: $2a$10$9FHhPyb7BW01ktwTTgZHX.>
+    # throw Exception.new(BCrypt::Engine.hash_secret(password, user.password_salt)) #uncaught throw #<Exception: $2a$10$9FHhPyb7BW01ktwTTgZHX.O62xalJit020Jb0g5XDdB5V8dGMslQS>
     if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
       user
-    else 
-      throw Exception.new("e nul")
+    else
       nil
     end
   end
@@ -27,3 +27,4 @@ class User < ActiveRecord::Base
     end
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Troublemap::Application.routes.draw do
-  get "log_out" => "sessions#destroy", :as => "log_out"
-  get "log_in" => "sessions#new", :as => "log_in"
-  get "sign_up" => "users#new", :as => "sign_up"
+  get "log_out" => "sessions#destroy", :as => :log_out
+  get "log_in" => "sessions#new", :as => :log_in
+  get "sign_up" => "users#new", :as => :sign_up
+  get "troubles/city/:id" => "troubles#city", :as => :city_trouble
   # get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
