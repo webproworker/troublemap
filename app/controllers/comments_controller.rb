d89448e2a966a040ac330b16be393a9863d2c2a5
webproworker <webproworker@gmail.com>
2013-10-15 12:40:22 +0300

    the main map is now zoomed on visitor's area.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,5 @@
 class CommentsController < ApplicationController
 
-  # http_basic_authenticate_with name: "test", password: "parola", only: :destroy
-
   def create
     @trouble = Trouble.find(params[:trouble_id])
     @comment = @trouble.comments.create(params[:comment].permit(:commenter, :body))
